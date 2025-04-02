48a1c61bb6369b214601cc4feed92c46b38ddcc0
Alejandro <alejandrorrnn@icloud.com>
2025-04-02 11:52:43 +0200

    removed leftovers

--- src/main/java/org/printed/chat/RubberDuckApplication.java
+++ src/main/java/org/printed/chat/RubberDuckApplication.java
@@ -2,7 +2,6 @@ package org.printed.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.printed.chat.interpreter.LispInterpreter;
 
 @SpringBootApplication
 public class RubberDuckApplication {
